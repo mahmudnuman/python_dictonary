965b9225920fcc13abe6b36bdbf92b1bb17882e7
mahmudnuman <mahmudnuman@gmail.com>
2022-11-18 23:46:59 +0600

    minor update

--- guardian_scrapper.py
+++ guardian_scrapper.py
@@ -14,7 +14,6 @@ with open('guardian_news.csv','w',encoding='utf8',newline='') as f:
     page=requests.get(url)
     soup=BeautifulSoup(page.content,"html.parser")
     lists=soup.find_all('h3',class_="fc-item__title")
-    t=0
     for list in lists:
         title=list.find('span',class_="js-headline-text").text
         url=list.find('a',class_="fc-item__link")["href"]
