4152dc53ccbb360d7408301823ee31c7ee0632fc
mahmudnuman <mahmudnuman@gmail.com>
2022-11-21 02:00:04 +0600

    new update

--- medex_scrapper_v3.py
+++ medex_scrapper_v3.py
@@ -3,10 +3,10 @@ import requests
 from csv import writer
 from datetime import datetime
 import time
+import random
 
-page =41
+page =10
 
-#this scrapper is incomplete, this comment will be removed when tested and done
 
 
 now = datetime.now() # current date and time
@@ -18,18 +18,20 @@ with open(name,'w',encoding='utf8',newline='') as f:
     header = ["Brand Name","Power","URL","Manufacturer","Unit Price"]
     thewriter.writerow(header)
     for p in range(1,page):
-        time.sleep(7)
+        num = random.randint(7, 20)
+
+        time.sleep(num)
         headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'}
         url = "https://medex.com.bd/brands?page=" +str(p)
         page = requests.get(url,headers=headers)
         soup = BeautifulSoup(page.content,'html.parser')   
         lists = soup.find_all('a',class_="hoverable-block")    
         for list in lists:            
-            brand_name = list.find('div',class_="data-row-top").text.strip()
-            power = list.find('span',class_="grey-ligten").text.strip()
+            brand_name = list.find('div',class_="data-row-top").get_text.strip()
+            power = list.find('span',class_="grey-ligten").get_text.strip()
             link = list['href']
-            manufacturer = list.find('span',class_="data-row-company").text.strip()
-            price = list.find('span',class_="package-pricing").text.strip() 
+            manufacturer = list.find('span',class_="data-row-company").get_text.strip()
+            price = list.find('span',class_="package-pricing").get_text.strip() 
             info = [brand_name,power,link,manufacturer,price]
             thewriter.writerow(info )
             
